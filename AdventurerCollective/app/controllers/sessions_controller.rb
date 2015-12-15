d36ba4f0dec685f60e3e7489a607825663933f93
Thomas <thomas.claiborne3@gmail.com>
2015-12-15 09:52:57 -0800

    Finish session controller

--- AdventurerCollective/app/controllers/sessions_controller.rb
+++ AdventurerCollective/app/controllers/sessions_controller.rb
@@ -3,11 +3,25 @@ class SessionsController < ApplicationController
   before_action :require_no_user!, only: [:new, :create]
 
   def new
+    render :new
   end
 
   def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user
+      login_user!(user)
+    else
+      flash.now[:errors] = ["Incorrect username and/or password"]
+      render :new
+    end
   end
 
   def destroy
+    logout_user!
+    redirect_to new_session_url
   end
 end
