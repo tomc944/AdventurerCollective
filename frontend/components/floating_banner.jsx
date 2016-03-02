eb52d030c2c2ceb87c37436b05478117dbe9ed29
Thomas <thomas.claiborne3@gmail.com>
2016-03-01 22:45:52 -0800

    Added scrolling text indicating what you can do on the site

--- frontend/components/floating_banner.jsx
+++ frontend/components/floating_banner.jsx
@@ -3,7 +3,18 @@ var React = require('react')
 var FloatingBanner = React.createClass({
   render: function() {
     return (
-      <p>Testing</p>
+      <div className="index-features">
+        <div className="index-features-visible">
+          <p className="index-features-perm-text">
+            With AdventurerCollective you can
+          </p>
+          <ul className="index-features-list">
+            <li className="index-features-list-item">find new adventures</li>
+            <li className="index-features-list-item">create adventures</li>
+            <li className="index-features-list-item">track your adventures and mark them completed</li>
+          </ul>
+        </div>
+      </div>
     );
   }
 });
